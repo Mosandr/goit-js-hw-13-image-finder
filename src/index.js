de2655e8131336cbc8722978890b0e8d0dac053a
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2021-03-15 11:49:48 +0200

    Refactor scroll to next page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,87 @@
 import './styles.css';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import ApiService from './js/apiService';
+import searchFormTemplate from './templates/searchFormTemplate.hbs';
+import imagesGalleryTemplate from './templates/imagesGalleryTemplate.hbs';
+import imageCardTemplate from './templates/imageCardTemplate.hbs';
+import loadMoreBtnTemplate from './templates/loadMoreBtnTemplate.hbs';
+const debounce = require('lodash.debounce');
+
+const apiService = new ApiService();
+
+const refs = {
+  bodyRef: document.querySelector('BODY'),
+};
+
+refs.bodyRef.insertAdjacentHTML('afterbegin', searchFormTemplate());
+refs.bodyRef.insertAdjacentHTML('beforeend', imagesGalleryTemplate());
+refs.bodyRef.insertAdjacentHTML('beforeend', loadMoreBtnTemplate());
+refs.searchForm = document.querySelector('.search-form');
+refs.gallery = document.querySelector('.gallery');
+refs.loadMoreBtn = document.querySelector('.load-more-btn');
+
+refs.searchForm.addEventListener('input', debounce(onQueryInput, 500));
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onQueryInput(event) {
+  event.preventDefault();
+  resetGallery();
+  apiService.resetPage();
+  apiService.searchQuery = event.target.value;
+  apiService.fetchImages().then(onFetchSucces).catch(onFetchError);
+}
+
+function onFetchSucces(data) {
+  if (data.length === 0) {
+    hideLoadMorebtn();
+    onFetchError();
+    return;
+  }
+  appendImagesCards(markupCardsList(data));
+  showLoadMorebtn();
+}
+
+function onFetchError() {
+  error({
+    text: 'Oops! No matches found',
+    delay: 1000,
+  });
+}
+
+async function onLoadMore(event) {
+  const y = refs.gallery.clientHeight + 30;
+  console.dir(y);
+  await apiService.fetchImages().then(onFetchSucces).catch(onFetchError);
+  window.scrollTo({
+    top: y,
+    left: 100,
+    behavior: 'smooth',
+  });
+}
+
+function appendImagesCards(listMarkup) {
+  refs.gallery.insertAdjacentHTML('beforeend', listMarkup);
+}
+
+function markupCardsList(data) {
+  const markup = data.reduce(
+    (acc, item) =>
+      acc + '<li class="gallery__item">' + imageCardTemplate(item) + '</li>',
+    '',
+  );
+  return markup;
+}
+
+function resetGallery() {
+  refs.gallery.innerHTML = '';
+}
+
+function showLoadMorebtn() {
+  refs.loadMoreBtn.classList.remove('hide');
+}
+
+function hideLoadMorebtn() {
+  refs.loadMoreBtn.classList.add('hide');
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,1 +1,42 @@
-export default class ApiService {}
+export default class ApiService {
+  constructor() {
+    this._searchQuery = '';
+    this._page = 1;
+  }
+
+  get searchQuery() {
+    return this._searchQuery;
+  }
+
+  set searchQuery(newQuery) {
+    this._searchQuery = newQuery;
+  }
+
+  get page() {
+    return this._page;
+  }
+
+  set page(newPage) {
+    this._page = newPage;
+  }
+
+  fetchImages() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const KEY = '20678815-cff8880680826357b58c80b80';
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+        return data.hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
